0bf63520c64d7db90356c9a962050f1c05d16423
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2023-10-13 17:25:02 +0200

    Initial release of mastodon-on-elk

---
+++ README.md
@@ -0,0 +1,39 @@
+<h1 align="center">Welcome to mastodon-on-elk 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: LGPL" src="https://img.shields.io/badge/License-LGPL-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/Riduidel" target="_blank">
+    <img alt="Twitter: Riduidel" src="https://img.shields.io/twitter/follow/Riduidel.svg?style=social" />
+  </a>
+</p>
+
+> A GreaseMonkey script to ensure all your Mastodon tabs open in elk. It uses a dynamic list obtained from instances.social
+
+### 🏠 [Homepage](https://github.com/Riduidel/mastodon-on-elk)
+
+## Install
+
+```sh
+Install GreaseMonkey or any script runner, then install script.
+```
+
+## Author
+
+👤 **Nicolas Delsaux**
+
+* Website: http://riduidel.wordpress.com
+* Twitter: [@Riduidel](https://twitter.com/Riduidel)
+* Github: [@Riduidel](https://github.com/Riduidel)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/Riduidel/mastodon-on-elk/issues). 
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/mastodon-on-elk.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name       mastodon-on-elk
+// @namespace  https://elk.zone/framapiaf.org/@Riduidel
+// @version    0.1
+// @description  Redirects all mastodon servers I know to be seen in the excellent https://elk.zone/ mastodon UI.
+// @match      https://*/*
+// @match      http://*/*
+// @noframes
+// @run-at document-end
+// @grant GM.listValues
+// @grant GM.getValue
+// @grant GM.setValue
+// @grant GM.xmlHttpRequest
+// ==/UserScript==
+
+const PREFIX = "mastodon-on-elk"
+const KEY = "mastodon-on-elk"
+
+function receiveServerList(response) {
+  console.info(PREFIX, "Received server list from GtiHub")
+  const servers = response.responseText.split("\n")
+  	.map(text => {
+    	if(text.indexOf("\"")==0) {
+        text = text.substring(1)
+      }
+      if(text.lastIndexOf("\"")==text.length-1) {
+        text = text.substring(0, text.length-1)
+      }
+      return text
+  	})
+
+  const config = {
+    "date": new Date(),
+    "servers": servers
+  }
+  console.info(PREFIX, "Obtained server list", config)
+  GM.setValue(KEY, JSON.stringify(config))
+}
+
+function fireRefreshServerList() {
+  GM.xmlHttpRequest({
+    method: "GET",
+    url: "https://raw.githubusercontent.com/Riduidel/mastodon-on-elk/servers/servers.json",
+    onload: receiveServerList
+  });
+}
+
+function ensureServerListIsFresh(mastodonOnElk) {
+  if(mastodonOnElk==null) {
+    fireRefreshServerList()
+  } else {
+    const config = JSON.parse(mastodonOnElk)
+    if(config.date<new Date().getTime()-1000*60*60*24*7) {
+      fireRefreshServerList()
+	    console.info(PREFIX, "There is a mastodon to elk config that is TOO OLD")
+    } else {
+	    console.info(PREFIX, "There is a mastodon to elk config that is young enough")
+    }
+  }
+}
+
+function redirectMastodonToElk(configText) {
+  const config = JSON.parse(configText)
+  console.info(PREFIX, "We have a valid mastodon config, testing server url", window.location, "⁉️")
+  const server = window.location.host
+  if(config.servers.includes(server)) {
+	  console.info(PREFIX, "Looks like a mastodon server! Redirecting! ⚡")
+  	window.location.href = `https://elk.zone/${server}${window.location.pathname}`
+  } else {
+    console.info(PREFIX, "This is not a mastodon server ❌")
+  }
+}
+
+GM.getValue(KEY)
+  	.then(ensureServerListIsFresh)
+	;
+GM.getValue(KEY)
+  	.then(redirectMastodonToElk)
+	;
